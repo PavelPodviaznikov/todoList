9611151f07f80df628b2f0ea169eac67290c8e1d
Pavel Podviaznikov <pavel.podviaznikov@gmail.com>
2014-09-25 16:10:34 +0300

    final version
    draggable
    delete button in "done tasks"
    delete img for every undone task

--- script.js
+++ script.js
@@ -4,31 +4,38 @@ var clickCounter;
 var done_counter = 0;
 
 $(document).ready(function() {
+    $('#container').draggable();
     //Работа табок
     $('.tabs .tab-links a').on('click', function(e)  {
         var currentAttrValue = $(this).attr('href');
         $('.tabs ' + currentAttrValue).siblings().slideUp(400);
         $('.tabs ' + currentAttrValue).delay(400).slideDown(400);
         $(this).parent('li').addClass('active').siblings().removeClass('active');
+        if($('#dons').attr('class')==="active"){
+            $('#delete_button').css("display", "inline-block");
+        }
+        else if($('#undons').attr('class')==="active"){
+            $('#delete_button').css("display", "none");
+        }
         e.preventDefault();
     });
+    
     //Добавление задания
-    $('#button').data('counter', 0).click(function(){
-        clickCounter = $(this).data('counter');
+    $('#button').click(function(){
+        clickCounter = undoneList.length;
         if($('#inputItem').val()===""){
              alert("Oops, try again!");
         }
         else{
             undoneList[clickCounter] = $('#inputItem').val();
-            $('#undone_tasks').append('<div class="item_container"><input type="checkbox" class="check_item"/><div class="undone_item">' + undoneList[clickCounter] + '</div></div>');
-            $(this).data('counter', clickCounter+1);
+            $('#undone_tasks').append('<div class="undone_item">' + undoneList[clickCounter] + '<div class="del_undone"><img src="img/delete-icon.png"></div></div>');
+            clickCounter=clickCounter+1;
             $('#undone_tasks_counter').text(undoneList.length);
-            console.log($('#inputItem').val());
         } 
     });
     //Удаление сделанных заданий
     $('#delete_button').on('click', function(){
-        $('#done_tasks').empty('.item');
+        $('#done_tasks').empty('.done_item');
         doneList = [];
         done_counter = 0;
         $('#done_tasks_counter').text(doneList.length);
@@ -38,15 +45,26 @@ $(document).ready(function() {
         var task = $(this).text();
         for(var i=0; i<undoneList.length; i+=1){
             if(undoneList[i]===task){
-                doneList.push(task);
-                //undoneList.splice(i,1);
-                $(this).replaceWith('<div class="undone_item"><strike>' + undoneList[i] + '</strike></div>');
-                $('#undone_tasks_counter').text(undoneList.length);
-                //$('#button').data('counter', clickCounter-1);   
+                $(this).replaceWith('<div class="undone_item"><strike>' + undoneList[i] + '</strike><div class="del_undone"><img src="img/delete-icon.png"></div></div>');
+                $('#undone_tasks_counter').text(undoneList.length);  
             }    
         }
+        doneList.push(task);
         $('#done_tasks').append('<div class="done_item">' + doneList[done_counter] + '</div>');
         done_counter+=1;
         $('#done_tasks_counter').text(doneList.length);
     }); 
+    $(document).on('click', '.del_undone', function(e){
+        e.stopPropagation();
+        for(var i=0; i<undoneList.length; i+=1){
+            if($(this).parent().text()===undoneList[i]){
+                undoneList.splice(i,1);
+                clickCounter=undoneList.length; 
+                break;  
+            }
+        }
+
+        $('#undone_tasks_counter').text(undoneList.length);
+        $(this).parent().remove();
+    });
 });
