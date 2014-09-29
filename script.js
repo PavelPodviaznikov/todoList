0b1b17fb9c89191c985c4f2fd67031f7144e921a
Pavel Podviaznikov <pavel.podviaznikov@gmail.com>
2014-09-29 13:59:23 +0300

    save and load without btns

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var doneList = [];
 var undoneList = [];
 var clickCounter;
-var done_counter = 0;
+var done_counter;
 var fb = new Firebase("https://podviaznikovtodolist.firebaseio.com/");
 var undoneRef = new Firebase("https://podviaznikovtodolist.firebaseio.com/undone");
 var doneRef = new Firebase("https://podviaznikovtodolist.firebaseio.com/done");
@@ -11,6 +11,22 @@ $(document).ready(function() {
     $('#container').draggable({
         revert: true
   });
+    undoneRef.once('value', function(allUndoneSnap){
+        allUndoneSnap.forEach(function(undoneSnap){
+            var undoneText = undoneSnap.child('task').val();
+            undoneList.push(undoneText);
+            $('#undone_tasks').append('<div class="undone_item">' + undoneText + '<div class="del_undone"><img src="img/delete-icon.png"></div></div>');
+            $('#undone_tasks_counter').text(undoneList.length);
+        });
+    });
+    doneRef.once('value', function(allDoneSnap){
+        allDoneSnap.forEach(function(doneSnap){
+            var doneText = doneSnap.child('task').val();
+            doneList.push(doneText);
+            $('#done_tasks').append('<div class="done_item">' + doneText + '</div>');
+            $('#done_tasks_counter').text(doneList.length);
+        });
+    });
     //Работа табок
     $('.tabs .tab-links a').on('click', function(e)  {
         var currentAttrValue = $(this).attr('href');
@@ -34,12 +50,17 @@ $(document).ready(function() {
         }
         else{
             undoneList[clickCounter] = $('#inputItem').val();
+            undoneRef.push({
+                task: undoneList[clickCounter]
+            });
             $('#undone_tasks').append('<div class="undone_item">' + undoneList[clickCounter] + '<div class="del_undone"><img src="img/delete-icon.png"></div></div>');
             clickCounter=clickCounter+1;
             $('#undone_tasks_counter').text(undoneList.length);
             $('#inputItem').val("");
+
         } 
     });
+    //Добавления зажания ентером
     $(document).keypress(function(e) {
     if(e.which == 13) {
         e.preventDefault();
@@ -49,6 +70,9 @@ $(document).ready(function() {
         }
         else{
             undoneList[clickCounter] = $('#inputItem').val();
+            undoneRef.push({
+                task: undoneList[clickCounter]
+            });
             $('#undone_tasks').append('<div class="undone_item">' + undoneList[clickCounter] + '<div class="del_undone"><img src="img/delete-icon.png"></div></div>');
             clickCounter=clickCounter+1;
             $('#undone_tasks_counter').text(undoneList.length);
@@ -66,6 +90,7 @@ $(document).ready(function() {
     });
     //Определение сделанных заданий
     $(document).on('click','#undone_tasks .undone_item', function(){
+        done_counter=doneList.length;
         var task = $(this).text();
         for(var i=0; i<undoneList.length; i+=1){
             if(undoneList[i]===task){
@@ -74,56 +99,30 @@ $(document).ready(function() {
             }    
         }
         doneList.push(task);
+        doneRef.push({
+            task: doneList[done_counter]
+        });
         $('#done_tasks').append('<div class="done_item">' + doneList[done_counter] + '</div>');
         done_counter+=1;
         $('#done_tasks_counter').text(doneList.length);
     }); 
+    //Удаление несделанных дел
     $(document).on('click', '.del_undone', function(e){
         e.stopPropagation();
         for(var i=0; i<undoneList.length; i+=1){
             if($(this).parent().text()===undoneList[i]){
                 undoneList.splice(i,1);
-                clickCounter=undoneList.length; 
+                clickCounter=undoneList.length;
                 break;  
             }
         }
-
         $('#undone_tasks_counter').text(undoneList.length);
         $(this).parent().remove();
-    });
-    $('#save').on('click', function(){
-        fb.child("undone").remove();
-        fb.child("done").remove();
-        var undoneRef = fb.child("undone");
-        var doneRef = fb.child("done");
-        for(var i=0; i<undoneList.length; i+=1){
+        undoneRef.remove();
+        for(var j=0; j<undoneList.length;j+=1){
             undoneRef.push({
-                task: undoneList[i]
-            });
+                task: undoneList[j]
+            }); 
         }
-        for(var j = 0; j<doneList.length; j+=1){
-            doneRef.push({
-                task: doneList[j]
-            });
-        }
-    });
-    $('#upload').one('click', function(){
-        undoneRef.once('value', function(allUndoneSnap){
-            allUndoneSnap.forEach(function(undoneSnap){
-                 var undoneText = undoneSnap.child('task').val();
-                 undoneList.push(undoneText);
-                 $('#undone_tasks').append('<div class="undone_item">' + undoneText + '<div class="del_undone"><img src="img/delete-icon.png"></div></div>');
-                 $('#undone_tasks_counter').text(undoneList.length);
-            });
-        });
-        doneRef.once('value', function(allDoneSnap){
-            allDoneSnap.forEach(function(doneSnap){
-                 var doneText = doneSnap.child('task').val();
-                 doneList.push(doneText);
-                 $('#done_tasks').append('<div class="done_item">' + doneText + '</div>');
-                 $('#done_tasks_counter').text(doneList.length);
-            });
-        });
-
     });
 });
